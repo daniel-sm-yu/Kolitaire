5140476782f86f054d48ea5cf4dac2ca671245a4
daniel-sm-yu <42286268+daniel-sm-yu@users.noreply.github.com>
2019-09-22 20:15:30 -0400

    initial commit

---
+++ src/GamePresenter.kt
@@ -0,0 +1,27 @@
+object GamePresenter {
+    var view: GameView? = null
+
+    fun setGameView(gameView: GameView) {
+        view = gameView
+    }
+
+    fun onDeckTap() {
+        GameModel.onDeckTap()
+        view?.update()
+    }
+
+    fun onWasteTap() {
+        GameModel.onWasteTap()
+        view?.update()
+    }
+
+    fun onFoundationTap(foundationIntex: Int) {
+        GameModel.onFoundationTap(foundationIntex)
+        view?.update()
+    }
+
+    fun onTableauTap(tableauIndex: Int, cardIndex: Int) {
+        GameModel.onTableauTap(tableauIndex, cardIndex)
+        view?.update()
+    }
+}
